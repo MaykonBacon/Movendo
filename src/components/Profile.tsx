93a9433009bdb6dcf387dc1dba799debeda2d4cb
MaykonBacon <32941006+MaykonBacon@users.noreply.github.com>
2021-02-25 09:38:49 -0300

    Código aula 04

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/componentes/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/MaykonBacon.png" alt="Maykon Soares"/>
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Maykon Soares</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import { ChallengesProvider, ChallengesContext } from '../contexts/ChallengesContext'
-import { useState } from 'react'
-
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChallengesProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </ChallengesProvider>
   )
 }
